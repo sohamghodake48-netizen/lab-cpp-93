47df18d9d120f9aff6d8f07828a6e053b4c43386
sohamghodake48-netizen <sohamghodake48@gmail.com>
2026-07-14 14:41:18 +0530

    Add files via upload

---
+++ 1st_question.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+     char name;
+     int div,rollno;
+     scanf("enter student name: %c",&name);
+     scanf("enter student rollno: %d",&rollno);
+     scanf("enter student div: %d",&div);
+     printf("student name: %c\t",name);
+     printf("student rollno: %d\t",rollno);
+     printf("student div: %d",div);
+     
+     
+}
