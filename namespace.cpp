f12fcda81ab33809001120b25b08c65abbc24156
sohamghodake48-netizen <sohamghodake48@gmail.com>
2026-07-21 08:58:42 +0000

    commit

---
+++ namespace.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum,avg;
+    cout<<"enter two numbers";
+    cin>>num1;
+    cin>>num2;
+    sum=num1+num2;
+    avg=sum/2;
+    cout<<"sum="<<sum<<"\t average="<<avg;
+    return 0;
+}
