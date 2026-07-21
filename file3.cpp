5e80cd04cb45b6ea91a00d6a9ec7e377f586e987
sohamghodake48-netizen <sohamghodake48@gmail.com>
2026-07-21 09:56:18 +0000

    commit

---
+++ file3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+namespace sycs4
+{
+    int sub()
+    {
+     int a,b,sub;
+     std::cout<<"enter two numbers";
+     std::cin>>a>>b;
+     std::cout<<"sum of two numbers is"<<(a-b);
+     return 0;
+    }
+}
---
+++ file4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+namespace sycs4
+{
+    int sub();
+}
+    int main()
+    {
+    sycs4::sub();
+    return 0;
+}
