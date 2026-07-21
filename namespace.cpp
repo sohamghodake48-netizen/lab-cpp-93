438c30892cc71e0c02f43436a5c71f2fd7bd8f17
sohamghodake48-netizen <sohamghodake48@gmail.com>
2026-07-21 09:16:49 +0000

    commit

--- namespace.cpp
+++ namespace.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
-using namespace std;
+//using namespace std;
 int main()
 {
     int num1,num2,sum,avg;
-    cout<<"enter two numbers";
-    cin>>num1;
-    cin>>num2;
+   std::cout<<"enter two numbers";
+   std::cin>>num1;
+    std::cin>>num2;
     sum=num1+num2;
     avg=sum/2;
-    cout<<"sum="<<sum<<"\t average="<<avg;
+    std::cout<<"sum="<<sum<<"\t average="<<avg;
     return 0;
 }
